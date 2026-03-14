501ec3e66f4ede731117c2df9e4a0b59bacaf305
ahm3dra2a <muhammad.ahmed.27@hotmail.com>
2026-03-14 07:48:39 +0500

    feat: Complete Stage 3 — implement REPL loop

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import sys
 
 
 def main():
-    # TODO: Uncommented the first line code below to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
