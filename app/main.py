12760a5588f22feba855cbcc617c097d4c28df33
ahm3dra2a <muhammad.ahmed.27@hotmail.com>
2026-03-14 07:09:04 +0500

    feat: Complete Stage 2 — handle invalid commands

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import sys
 def main():
     # TODO: Uncommented the first line code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    sys.stdout.flush()
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
